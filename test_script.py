1b5f6611ec06f9f706eaf5590e6b02453a94fd89
hyunminboo <boohyunmin@gmail.com>
2026-06-03 22:26:20 +0900

    Remove test scripts from tracking

--- test_script.py
+++
@@ -1,9 +0,0 @@
-class A:
-    x: int
-    y: int = 1
-class Meta(type):
-    def __new__(mcs, name, bases, namespace):
-        print("namespace keys:", list(namespace.keys()))
-        return super().__new__(mcs, name, bases, namespace)
-class B(metaclass=Meta):
-    z: str = "hello"
--- test_script2.py
+++
@@ -1,8 +0,0 @@
-class Meta(type):
-    def __new__(mcs, name, bases, namespace):
-        print("namespace keys:", list(namespace.keys()))
-        print("annotations:", namespace.get("__annotations__"))
-        return super().__new__(mcs, name, bases, namespace)
-class B(metaclass=Meta):
-    __annotations__ = {"z": str}
-    z: str = "hello"
